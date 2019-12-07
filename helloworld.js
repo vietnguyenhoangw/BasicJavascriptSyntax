b4e019ae97dff3f718297c01e0b650d7671446ea
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-12-07 11:47:31 +0700

    :pencil: first commit

---
+++ helloworld.js
@@ -0,0 +1,59 @@
+//----------- Part 1
+// variable and declare variable
+var username = 100
+var __username
+var __username90 = 'daniel nguyen'
+
+__username = 2.4
+
+//----------- Part 2
+// operator
+var a = 20
+var b = 5
+var c = a + b	// 25
+var d = a - b	// 15
+var e = a * b	// 100
+var f = a / b	// 4
+var g = 22 % b	// 2
+
+//console.log(a++) - get recent value (a = 20) and add 1
+++b		// - add 1 and get value (a = 6)
+//similar with --
+
+//----------- Part 3
+// assignment operator (toan tu gan)
+var a_2 = 12
+var b_2 = a_2 	// b_2 = 12
+b_2 += a_2 	// b_2 = 24 (b_2 = b2 + a_2)
+b_2 -= a_2	// b_2 = 12
+b_2 *= a_2	// b_2 = 144
+b_2 /= a_2	// b_2 = 12
+b_2 %= a_2	// b_2 = 0
+
+//----------- Part 4
+// relationship operator
+var a_3  = 12;
+var b_3 = 20;
+var c_3 = a == b; // False
+var d_3 = a > b;  // False
+var e_3 = a >= b; // False
+var f_3 = a < b;  // True
+var g_3 = a <= b; // False
+var h_3 = a != b; // True
+
+
+//----------- Part 5
+// logical operator
+var a_4 = false;
+var b_4 = true;
+var c_4 = a && b; // TRUE
+var d_4 = a || b; // TRUE
+var e_4 = !a;	  // TRUE
+var f_4 = !b;	  // FALSE
+
+
+//----------- Part 6
+// equal operator (NOTE)
+
+//return value to console
+console.log(c_3)
